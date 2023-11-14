e28b3e5dcac69ee5faa9aa05519ba5d1cac44e7d
Junghoon Lee <114772245+jhlee-uppi@users.noreply.github.com>
2023-11-14 10:29:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -139,8 +139,8 @@ with tab1:
             font_color="black",
             font_family="arial",
             font_weight="bold",
-            text-align = "center",
-            vertical-align= "middle"
+            text_align = "center",
+            vertical_align= "middle"
         )
         
         m2.to_streamlit(height=400, )
